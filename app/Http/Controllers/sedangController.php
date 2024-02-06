000851696deaf7eb699438137ce0a40660aef981
ReyChan44 <154233349+ReyChan44@users.noreply.github.com>
2024-02-07 02:14:19 +0700

    Add files via upload

---
+++ app/Http/Controllers/sedangController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class sedangController extends Controller
+{
+    function sedang() {
+        return view('sedang');
+    }
+}
+ 
